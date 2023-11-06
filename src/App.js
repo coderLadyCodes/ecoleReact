8665e1414fee0d0a8a0ce52e2ab2c4553b6d0d76
coderLady <creativegirl@outlook.fr>
2023-11-06 13:45:44 +0100

    texte

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '/node_modules/bootstrap/dist/js/bootstrap.min.js';
 import './App.css';
+import Home from './Home';
 import UsersView from './component/user/UsersView';
+import NavBar from './component/common/NavBar';
+import { BrowserRouter  as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-    <h2>welcome to ecole front end</h2> 
-    <UsersView />
+    <Router>
+    <NavBar /> 
+      <Routes>
+        <Route exact path="/" element={<Home />}>
+        </Route>
+      </Routes>
+    </Router>
+
+    <Router>
+      <Routes>
+        <Route exact path="/view-users" element={<UsersView />}>
+        </Route>
+      </Routes>
+    </Router>
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <h1>School Home page Website</h1>
+      
+    </div>
+  )
+}
+
+export default Home
