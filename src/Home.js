a6574b2e8b7a7566ddfbe10c4ecfd547d1da8da7
coderLady <creativegirl@outlook.fr>
2023-11-12 13:08:12 +0100

    img

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from 'react';
+import homeBackground from './images/homeBackground.jpg';
 
 function Home() {
   return (
-    <div>
-        <h1>School Home page Website</h1>
-      
+    <div className='container'>
+      <img className='img-fluid'
+      src={homeBackground}
+      style={{maxWidth: '100%', height:'auto'}}
+      alt='Home Page Image'
+      />   
     </div>
   )
 }
