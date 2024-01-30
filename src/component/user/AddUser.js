7dc47166a943455017dfcaadb269586cab13f513
coderLady <creativegirl@outlook.fr>
2024-01-30 11:07:50 +0100

    add

--- src/component/user/AddUser.js
+++ src/component/user/AddUser.js
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
 import {Link, useNavigate} from 'react-router-dom';
 import axios from 'axios';
-
 const AddUser = () => {
 
   let navigate = useNavigate();
-
+  
+  const [errors, setErrors] = useState({
+    name:'',
+    email:'',
+    phone:'',
+    multipartFile:''
+  })
   const [userDTO, setUserDTO] = useState({
     name: '',
     email: '',
@@ -43,29 +48,88 @@ const AddUser = () => {
     } catch (error) {
       console.error('Error:', error);
     }
-  };
-
-  return (
-    <form onSubmit={handleSubmit} encType="multipart/form-data" method='post'>
-      <div>
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" name="name" onChange={handleInputChange} value={userDTO.name} />
-      </div>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input type="text" id="email" name="email" onChange={handleInputChange} value={userDTO.email} />
-      </div>
-      <div>
-        <label htmlFor="phone">Phone:</label>
-        <input type="text" id="phone" name="phone" onChange={handleInputChange} value={userDTO.phone} />
-      </div>
-      <div>
-        <label htmlFor="file">Profile Image:</label>
-        <input type="file" id="file" name="file" onChange={handleFileChange} />
-      </div>
-      <button type="submit">Create User</button>
-    </form>
-  );
+  
+  /*function validateForm(){
+    let valid = true;
+    const errorsCopy = {...errors};
+  
+    if(name.trim()){
+      errorsCopy.name = '';
+    }else {
+      errorsCopy.name = 'Le Nom est Obligatoire';
+      valid = false;
+    }
+  
+    if(email.trim()){
+      errorsCopy.email = '';
+    }else {
+      errorsCopy.email = 'Email est Obligatoire';
+      valid = false;
+    }
+  
+    if(phone.trim()){
+      errorsCopy.phone = '';
+    }else {
+      errorsCopy.phone = 'Le Numéro de Tél est Obligatoire';
+      valid = false;
+    }
+    return valid;
+  }*/
 };
-
-export default AddUser;
+    return (
+      <div className="container">
+        <div className="row">
+      <div className="d-flex justify-content-center">
+      <div className="lg-4">
+        <div className="card">
+        <div className="card-body text-center">
+  
+        <h2 className='mb-5'>Add User</h2>
+  
+        <form onSubmit={handleSubmit} encType="multipart/form-data" method='post'>  {/* (e) => saveUser(e)*/} 
+  
+          <div className='input-group mb-5'>
+            <label className='input-group-text' htmlFor='name'>Nom et Prénom</label>
+            <input autoComplete="name" placeholder='Nom et Prénom' className='form-control col-sm-6' type='text' name='name' id='name'  onChange={handleInputChange} value={userDTO.name} />
+          </div>
+  
+          <div className='input-group mb-5'>
+            <label className='input-group-text' htmlFor='email'>Email</label>
+            <input autoComplete="email" placeholder='Email' className='form-control col-sm-6' type='email' name='email' id='email'  onChange={handleInputChange} value={userDTO.email}/>
+          </div>
+  
+          <div className='input-group mb-5'>
+            <label className='input-group-text' htmlFor='phone'>Numéro de Téléphone</label>
+            <input autoComplete="tel" placeholder='Numero de Telephone' className='form-control col-sm-6' type='number' name='phone' id='phone'  onChange={handleInputChange} value={userDTO.phone}/>
+          </div>
+  
+          
+          <div className='input-group mb-5'>
+            <label className='input-group-text' htmlFor='multipartFile'>Choisir une Photo</label>
+            <input className='form-control col-sm-6' type='file' name='multipartFile' id='multipartFile' accept="image/*"   onChange={handleFileChange}/>
+          </div>
+  
+        {/*  <div className='input-group mb-5'>
+            <label className='input-group-text' htmlFor='role'>Role</label>
+            <input className='form-control col-sm-6' type='role' name='role' id='role' required value={role} onChange={(e) => handleInputChange(e)}/>
+          </div>*/}
+  
+          <div className='row mb-5'>
+            <div className='col-sm-6 p-4'>
+              <button type='submit' className='btn btn-outline-success btn-ls'>Save</button>
+            </div>
+            <div className='col-sm-4 p-4'>
+              <Link to={"/view-users"}  type='submit' className='btn btn-outline-warning btn-ls'>Cancel</Link> 
+            </div>
+          </div>
+        </form>   
+      </div>
+      </div>
+      </div>
+      </div>
+      </div>
+      </div>
+    );
+  };
+  
+  export default AddUser;
