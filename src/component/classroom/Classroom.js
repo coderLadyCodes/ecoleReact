ee5fe7bb2a9a81fedd973c98990a24fe9662079e
coderLady <creativegirl@outlook.fr>
2024-07-23 21:48:32 +0200

    ch

--- src/component/classroom/Classroom.js
+++ src/component/classroom/Classroom.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { useAuth } from '../user/AuthProvider'
 
 const Classroom = () => {
     const {id} = useParams()
+    const {userId, user, userName} = useAuth()
+    const [classroom, setClassroom] = useState({
+      grade:'',
+      userId:userId,
+      classroomCode:'',
+    })
   return (
-    <div>Classroom</div>
+    <div>
+      <h1>Classe de {userName}</h1>
+    </div>
   )
 }
 
